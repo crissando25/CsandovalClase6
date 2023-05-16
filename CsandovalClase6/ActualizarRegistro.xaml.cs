271bd63dc99c7bff8bfbd19f71bc44e3ede6816a
cristian sandoval <131212494+crissando25@users.noreply.github.com>
2023-05-16 17:56:49 -0500

    actualizacion de datos

--- CsandovalClase6/ActualizarRegistro.xaml.cs
+++ CsandovalClase6/ActualizarRegistro.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,21 +19,19 @@ namespace CsandovalClase6
             InitializeComponent();
         }
 
-        private async void BtnActualizar_Clicked(object sender, EventArgs e)
+        private async void BtnActualizar_Clicked(object sender, EventArgs e, string urlRequest)
         {       
             HttpClient client = new HttpClient();
             var content = new DatosRequest
             {
-                id = Convert.ToInt32(text.Text),
-               brand = TxtMarca.Text,
-                model = TxtModelo.Text,
-                year = Convert.ToInt32(TxtAnio.Text),
-                plate = TxtPlaca.Text,
-                color = TxtColor.Text,
+                id = Convert.ToInt32(txtTbl_jz_id.Text),
+                tbl_jz_nombre = txttbl_jz_nombre.Text,
+                tbl_jz_grupo_t = txttbl_jz_grupo_t.Text,
+                tbl_g_observaciones = txttbl_g_observaciones.Text,
             };
             string jsonData = JsonConvert.SerializeObject(content);
             var vehicleRequest = new StringContent(jsonData, Encoding.UTF8, "application/json");*/
-            string urlRequest = $"{Url}?id={content.id}&brand={content.brand}&model={content.model}&year={content.year}&plate={content.plate}&color={content.color}";
+            string urlRequest; = $"{"http://192.168.1.26/moviles/post.php"}?id={content.id}&Nombre={content.tbl_jz_nombre}&Grupo={content.tbl_jz_grupo_t}&observaciones={content.tbl_g_observaciones};
             var response = await client.PutAsync(urlRequest, null); //vehicleRequest si usamos content en el servicio php
             if (response.IsSuccessStatusCode)
             {
---
+++ CsandovalClase6/DatosRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CsandovalClase6
+{
+    internal class DatosRequest
+    {
+        internal object brand;
+
+        public int id { get; set; }
+        public string tbl_jz_nombre { get; set; }
+        public string tbl_jz_grupo_t { get; set; }
+        public string tbl_g_observaciones { get; set; }
+    }
+}
